437f8e37b6652339cf5b98fa171fc0703b8b4bb5
shishir28 <shishir28@gmail.com>
2018-12-29 16:28:05 +1100

    Scheduled function added

---
+++ azure-functions/ToDoFunction/ToDoFunctionApp/ScheduledFunction.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ToDoFunctionApp
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async System.Threading.Tasks.Task RunAsync([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, 
+            [Table("todos", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var query = new TableQuery<ToDoTableEntity>();
+            var segment = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            var deleted = 0;
+            foreach (var todo in segment)
+            {
+                if (todo.IsCompleted)
+                {
+                    await todoTable.ExecuteAsync(TableOperation.Delete(todo));
+                    deleted++;
+                }
+
+            }
+            log.LogInformation($"Delete {deleted} item at {DateTime.Now}");
+
+
+
+        }
+    }
+}
