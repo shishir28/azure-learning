aead3e923214b96ef42adfc53045bca6f28c8f07
shishir28 <shishir28@gmail.com>
2019-04-02 10:56:50 +1100

    preparatory work for APIM

---
+++ api-management/TaskAPI/scripts/az-cli-setup.ps1
@@ -0,0 +1,14 @@
+﻿az  login
+
+
+$resourceGroup = "apimanagementrg"
+$location = "australiaeast"
+
+
+# Create resource group
+az  group create -n $resourceGroup -l $location
+
+$webApp = "shishirtaskapi"
+
+
+
